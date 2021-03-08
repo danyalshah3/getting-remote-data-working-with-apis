e29ac5d62edf7d150260b01fcfb4a692520b9b58
MuhammadDanyalMahdi <danyalm50@gmail.com>
2021-03-08 06:22:59 -0500

    Done.

--- lib/nyc_api.rb
+++ lib/nyc_api.rb
@@ -6,6 +6,14 @@ class GetPrograms
 
   URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
 
+  def program_school
+    # we use the JSON library to parse the API response into nicely formatted JSON
+      programs = JSON.parse(self.get_programs)
+      programs.collect do |program|
+        program["agency"]  
+      end
+    end 
+
   def get_programs
     uri = URI.parse(URL)
     response = Net::HTTP.get_response(uri)
@@ -14,5 +22,8 @@ class GetPrograms
 
 end
 
-programs = GetPrograms.new.get_programs
-puts programs
+# programs = GetPrograms.new.get_programs
+# puts programs
+
+programs = GetPrograms.new
+puts programs.program_school.uniq 
